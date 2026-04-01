9ea31f181f0b810db1094c4c1968faeb1bd67505
andoh <solomonkingsman@gmail.com>
2026-04-01 21:27:57 +0000

    fix postgres driver issues

--- src/db/session.py
+++ src/db/session.py
@@ -1,8 +1,8 @@
 from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
 
-from config import settings
+from src.config import settings
 
 
 engine = create_async_engine(settings.database_url, echo=True)
 
-AsyncSessionLocal = async_sessionmaker(engine, expire_on_commit=False)
+AsyncSessionLocal = async_sessionmaker(bind=engine, expire_on_commit=False)
