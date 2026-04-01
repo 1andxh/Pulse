27816030829a70c58256a834d8d4d82f36ee739e
andoh <solomonkingsman@gmail.com>
2026-04-01 22:27:20 +0000

    create monitor model

---
+++ src/monitor/__init__.py
@@ -0,0 +1,3 @@
+from .models import Monitor, Probe
+
+__all__ = ["Monitor", "Probe"]
---
+++ src/monitor/models.py
@@ -0,0 +1,27 @@
+from sqlalchemy.orm import mapped_column, Mapped, relationship
+from sqlalchemy import ForeignKey, DateTime, String, Boolean
+from src.db.base import Base
+import uuid
+from sqlalchemy.dialects.postgresql import UUID
+
+from datetime import datetime, timezone
+from src.probe.models import Probe
+
+
+class Monitor(Base):
+    # specific target to watch
+
+    __tablename__ = "monitors"
+
+    id: Mapped[uuid.UUID] = mapped_column(
+        UUID(as_uuid=True), primary_key=True, default=uuid.uuid4
+    )
+    name: Mapped[str] = mapped_column(String(255), nullable=False)
+    url: Mapped[str] = mapped_column(String(1024), nullable=False)
+    is_active: Mapped[bool] = mapped_column(Boolean, default=True)
+    check_interval: Mapped[int] = mapped_column(default=30)
+
+    # relationships
+    probes: Mapped[list["Probe"]] = relationship(
+        "Probe", back_populates="monitor", cascade="all, delete-orphan"
+    )
