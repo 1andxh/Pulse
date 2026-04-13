05dce5da88bbd5ad851e4c4a25b9e6638bf6e7d7
andoh <solomonkingsman@gmail.com>
2026-04-13 13:51:02 +0000

    add global exception andlers

---
+++ src/exception_handler.py
@@ -0,0 +1,30 @@
+from fastapi import FastAPI, Request, status
+from fastapi.responses import JSONResponse
+from fastapi.exceptions import RequestValidationError
+
+from src.execptions import DomainError
+
+
+async def writewinged_exception_handler(request: Request, exc: DomainError):
+    return JSONResponse(
+        status_code=exc.status_code,
+        content={"error": exc.__class__.__name__, "message": exc.message},
+    )
+
+
+async def validation_exception_handler(request: Request, exc: RequestValidationError):
+    return JSONResponse(
+        status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
+        content={"error": "ValidationError", "message": exc.errors()},
+    )
+
+
+async def general_exception_handler(request: Request, exc: Exception):
+    # Log the actual 'exc' here so you can debug it!
+    return JSONResponse(
+        status_code=500,
+        content={
+            "error": "InternalServerError",
+            "message": "An unexpected error occurred.",
+        },
+    )
