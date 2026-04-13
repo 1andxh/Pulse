58031638789b6a22736628a89fecd769b884bbfa
andoh <solomonkingsman@gmail.com>
2026-04-13 14:46:36 +0000

    create session instance

--- src/db/session.py
+++ src/db/session.py
@@ -1,8 +1,24 @@
-from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
+from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncSession
+from typing import AsyncGenerator
 
 from src.config import settings
 
 
 engine = create_async_engine(settings.database_url, echo=True)
 
-AsyncSessionLocal = async_sessionmaker(bind=engine, expire_on_commit=False)
+AsyncSessionLocal = async_sessionmaker(
+    bind=engine, expire_on_commit=False, class_=AsyncSession
+)
+
+
+async def get_session() -> AsyncGenerator[AsyncSession, None]:
+
+    async with AsyncSessionLocal() as session:
+        try:
+            yield session
+            await session.commit()
+        except Exception:
+            await session.rollback()
+            raise
+        finally:
+            await session.close()
