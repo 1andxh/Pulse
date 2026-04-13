b7105213a2c5c29c2e13c596107078ab12996b51
andoh <solomonkingsman@gmail.com>
2026-04-13 03:29:26 +0000

    change model id to uuid

--- src/probe/models.py
+++ src/probe/models.py
@@ -15,12 +15,14 @@ class Probe(Base):
 
     __table_args__ = (Index("ix_probe_monitor_timestamp", "monitor_id", "timestamp"),)
 
-    id: Mapped[int] = mapped_column(primary_key=True)
+    id: Mapped[uuid.UUID] = mapped_column(
+        UUID(as_uuid=True), primary_key=True, default=uuid.uuid4
+    )
     monitor_id: Mapped[uuid.UUID] = mapped_column(
         UUID(as_uuid=True), ForeignKey("monitors.id"), nullable=False
     )
-    timestampt: Mapped[datetime] = mapped_column(
-        DateTime(timezone=True), server_default=func.now(), nullable=False
+    timestamp: Mapped[datetime] = mapped_column(
+        DateTime(timezone=True), server_default=func.now(), nullable=False, index=True
     )
     is_up: Mapped[bool] = mapped_column(Boolean, default=True)
     latency_ms: Mapped[float | None] = mapped_column(nullable=True)
