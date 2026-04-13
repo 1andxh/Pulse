4d7475718beac6331989d98ec3ec71eeaf458d67
andoh <solomonkingsman@gmail.com>
2026-04-13 03:30:02 +0000

    update schemas

--- src/monitor/schemas.py
+++ src/monitor/schemas.py
@@ -1,10 +1,26 @@
 from pydantic import BaseModel, HttpUrl
+import uuid
 
 
 class MonitorCreate(BaseModel):
     name: str
     url: HttpUrl
     check_interval: int = 30
+
+
+class MonitorUpdate(BaseModel):
+    name: str | None
+    url: HttpUrl | None
+    is_active: bool | None
+    check_interval: int | None
+
+
+class MonitorRead(BaseModel):
+    id: uuid.UUID
+    name: str
+    url: HttpUrl
+    is_active: bool
+    check_interval: int
 
 
 # field validator for strict https?
