e91c0230d4abf03606823204e1e55ff43f8001cf
andoh <solomonkingsman@gmail.com>
2026-04-13 13:50:37 +0000

    add domain exceptions

---
+++ src/exceptions.py
@@ -0,0 +1,13 @@
+from fastapi import status
+
+
+class DomainError(Exception):
+    def __init__(self, message: str, status_code: int) -> None:
+        self.message = message
+        self.status_code = status_code
+        super().__init__(self.message, self.status_code)
+
+
+class DuplicateMonitorError(DomainError):
+    def __init__(self) -> None:
+        super().__init__("Monitor already exists", status_code=status.HTTP_409_CONFLICT)
