1c328c98ef814fe5c97e82970a55cc9a36f1811b
andoh <solomonkingsman@gmail.com>
2026-04-14 23:04:19 +0000

    update all route handlers

--- src/monitor/routes.py
+++ src/monitor/routes.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter, status, Depends
 from typing import Annotated
-from .schemas import MonitorRead, MonitorCreate
+from .schemas import MonitorRead, MonitorCreate, MonitorUpdate
 from .services import MonitorService
 from .dependency import get_monitor_service
 from src.db.session import get_session
@@ -21,6 +21,26 @@ async def create_monitor_route(payload: MonitorCreate, service: _service):
     return monitor
 
 
-@monitor_router.get("/{monitor_id}", response_model=MonitorRead)
-async def get_monitor(id: uuid.UUID, service: _service):
-    return await service.get_monitor_by_id(id)
+@monitor_router.get("/{monitor_monitor_id}", response_model=MonitorRead)
+async def get_monitor(monitor_id: uuid.UUID, service: _service):
+    return await service.get_monitor_by_id(monitor_id)
+
+
+@monitor_router.get("", response_model=list[MonitorRead])
+async def list_monitors(service: _service):
+    monitors = await service.get_all_monitors()
+    return monitors
+
+
+@monitor_router.patch("/{monitor_monitor_id}", response_model=MonitorRead)
+async def update_monitor(
+    monitor_id: uuid.UUID,
+    payload: MonitorUpdate,
+    service: _service,
+):
+    return await service.update_monitor(monitor_id, payload)
+
+
+@monitor_router.delete("/{monitor_monitor_id}", status_code=status.HTTP_204_NO_CONTENT)
+async def delete_monitor(monitor_id: uuid.UUID, service: _service):
+    return await service.delete_monitor(monitor_id)
