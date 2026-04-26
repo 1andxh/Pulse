769ad94b8e1519227c33307991e973a6ba2f990a
andoh <solomonkingsman@gmail.com>
2026-04-26 23:26:52 +0000

    implement dependecy injection

--- src/monitor/dependency.py
+++ src/monitor/dependency.py
@@ -1,6 +1,10 @@
 from .services import MonitorService
 from sqlalchemy.ext.asyncio import AsyncSession
+from src.db.session import get_session
+from fastapi import Depends
 
 
-async def get_monitor_service(session: AsyncSession) -> MonitorService:
+async def get_monitor_service(
+    session: AsyncSession = Depends(get_session),
+) -> MonitorService:
     return MonitorService(session)
